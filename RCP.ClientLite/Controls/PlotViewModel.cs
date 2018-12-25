43a1f000a9435259106262401ca6cedf2adb467b
Patrick Piech <piechpatrick@gmail.com>
2018-12-25 10:10:12 +0100

    Chart fix

--- RCP.ClientLite/Controls/PlotViewModel.cs
+++ RCP.ClientLite/Controls/PlotViewModel.cs
@@ -1,17 +1,21 @@
 ﻿using OxyPlot;
 using OxyPlot.Axes;
 using OxyPlot.Series;
+using Prism.Commands;
 using Prism.Mvvm;
 using RCP.Core;
 using System;
 using System.Linq;
+using System.Windows.Input;
 
 namespace RCP.ClientLite.Controls
 {
     public class PlotViewModel : BindableBase
     {
-        private PlotModel plotModel;
 
+        private ICommand refreshCommand;
+        private PlotModel plotModel;
+        private string overall;
 
 
         public PlotViewModel()
@@ -39,12 +43,21 @@ namespace RCP.ClientLite.Controls
             {
                 MajorGridlineStyle = LineStyle.Solid,
                 MinorGridlineStyle = LineStyle.Dot,
-                Title = "Minuty", 
+                Title = "Godziny", 
                 Position = AxisPosition.Left
             };
             PlotModel.Axes.Add(valueAxis);
 
             this.LoadData();
+        }
+
+        public ICommand RefreshCommand => this.refreshCommand == null
+            ? refreshCommand = new DelegateCommand(this.LoadData) : this.refreshCommand;
+
+        public string Overall
+        {
+            get { return this.overall; }
+            set { this.SetProperty(ref overall, value); }
         }
 
         public PlotModel PlotModel
@@ -55,27 +68,27 @@ namespace RCP.ClientLite.Controls
 
         private void LoadData()
         {
+            PlotModel.Series.Clear();
             var activities = Kernel.Instance.ActivityRepository.GetAll();
-            //var gr = activities.GroupBy(a => a.StartDate);
-
 
             foreach (var data in activities)
             {
                 var lineSerie = new LineSeries
                 {
+                    LineStyle = LineStyle.Automatic,
                     StrokeThickness = 2,
                     MarkerSize = 3,
                     MarkerStroke = OxyColors.Blue,
                     MarkerType = MarkerType.Square,
-                    CanTrackerInterpolatePoints = false,
+                    CanTrackerInterpolatePoints = true,
                     Title = data.Name,
-                    Smooth = false,
+                    Smooth = true,
                 };
-                lineSerie.Points.Add(new DataPoint(DateTimeAxis.ToDouble(data.StartDate), Math.Round((data.EndDate - data.StartDate).TotalMinutes,2)));
+                lineSerie.Points.Add(new DataPoint(DateTimeAxis.ToDouble(data.StartDate), Math.Round((data.EndDate - data.StartDate).TotalHours,2)));
                 PlotModel.Series.Add(lineSerie);
             }
-
-
+            this.PlotModel.ResetAllAxes();
+            this.Overall = $" Godziny:  {activities.Select(a => Math.Round((a.EndDate - a.StartDate).TotalHours, 2)).Sum().ToString()}";
         }
     }
 }
